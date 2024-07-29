53c82de4c4bd058a90d90a5cbc1c02cb1f04c399
JaleneHailu <147465004+JaleneHailu@users.noreply.github.com>
2024-07-29 11:00:12 +0300

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://amazon-api-deploy-5vg1.onrender.com',
+    baseURL: 'https://amazon-api-deploy-1-yufe.onrender.com/',
     headers: {
         'Content-Type': 'application/json',
     },
